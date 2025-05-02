87c417806741c0f3917ca5f2687d7a9728e730e5
Vishnu Patel <viptl123@gmail.com>
2025-05-02 01:50:40 -0500

    added extra credit files

--- crypto_utils_extra_credit.py
+++ crypto_utils_extra_credit.py
@@ -3,12 +3,13 @@ from Crypto.Random import get_random_bytes
 from Crypto.Protocol.KDF import PBKDF2
 
 
-
+# Uses AES ECB mode with the shared key that was negotiated between server and client
 def encrypt_message(message, key):
     cipher = AES.new(key, AES.MODE_ECB)
     padded_message = message.encode() + (b'\0' * (AES.block_size - len(message.encode()) % AES.block_size))
     ciphertext = cipher.encrypt(padded_message)
     return ciphertext
+
 
 def decrypt_message(ciphertext, key):
     cipher = AES.new(key, AES.MODE_ECB)
--- server.py
+++ server.py
@@ -17,11 +17,11 @@ def main():
     salt = b'secure_salt_1234'
     key = derive_key(password, salt)
 
-    s = socket.socket()
-    s.bind(('0.0.0.0', 5000))
-    s.listen(1)
+    sock = socket.socket()
+    sock.bind(('0.0.0.0', 5000))
+    sock.listen(1)
     print("[Waiting for connection...]")
-    conn, addr = s.accept()
+    conn, addr = sock.accept()
     print(f"[Connected to]: {addr}")
 
     threading.Thread(target=handle_client, args=(conn, key)).start()
