2c80b0b224232bf01abcf6a96449f3bc3e011bdd
Vishnu Patel <viptl123@gmail.com>
2025-05-04 22:25:05 -0500

    changed v2

--- p2p_gui.py
+++ p2p_gui.py
@@ -13,20 +13,20 @@ class SecureChatApp:
         self.conn = None
         self.alive = True
         self.master.protocol("WM_DELETE_WINDOW", self.on_close)
-        self.text_area = scrolledtext.ScrolledText(master, wrap=tk.WORD, height=20, width=50)
+        self.text_area = scrolledtext.ScrolledText(master, wrap=tk.WORD, height=25, width=62)
         self.text_area.pack()
         self.text_area.config(state=tk.DISABLED)
 
         self.entry = tk.Entry(master, width=40)
         self.entry.pack(side=tk.LEFT, padx=5, pady=5)
         self.send_btn = tk.Button(master, text="Send", command=self.send_message)
-        self.send_btn.pack(side=tk.RIGHT, padx=5, pady=5)
+        self.send_btn.pack(side=tk.RIGHT, padx=6, pady=6)
 
         self.password = simpledialog.askstring("Password", "Enter shared password:", show="*")
         if not self.password:
             self.master.destroy()
             return
-        self.salt = b'secure_salt_1234'
+        self.salt = b'this_is_our_salt'
         self.key = derive_key(self.password, self.salt)
 
         threading.Thread(target=self.network_thread).start()
@@ -41,9 +41,9 @@ class SecureChatApp:
                 pass
         self.master.destroy()
 
-    def append_text(self, msg):
+    def append_text(self, text):
         self.text_area.config(state=tk.NORMAL)
-        self.text_area.insert(tk.END, msg + "\n")
+        self.text_area.insert(tk.END, text + "\n")
         self.text_area.config(state=tk.DISABLED)
         self.text_area.see(tk.END)
 
@@ -58,15 +58,15 @@ class SecureChatApp:
         else:
             self.conn = socket.socket()
             self.conn.connect(("localhost", 5000))
-            self.append_text("Connected to server.")
+            self.append_text("Connected.")
 
         self.conn.settimeout(1.0)
         while self.alive:
             try:
                 data = self.conn.recv(4096).decode()
                 if data:
-                    decrypted = decrypt_message(data, self.key)
-                    self.append_text(f"[Friend] {decrypted} (Encrypted: {data})")
+                    decrypted_message = decrypt_message(data, self.key)
+                    self.append_text(f"[Friend] {decrypted_message} (Encrypted: {data})")
             except socket.timeout:
                 continue
             except:
@@ -75,9 +75,9 @@ class SecureChatApp:
     def send_message(self):
         msg = self.entry.get()
         if msg and self.conn:
-            enc = encrypt_message(msg, self.key)
-            self.conn.send(enc.encode())
-            self.append_text(f"[You] {msg} (Encrypted: {enc})")
+            encrypted_message = encrypt_message(msg, self.key)
+            self.conn.send(encrypted_message.encode())
+            self.append_text(f"[You] {msg} (Encrypted: {encrypted_message})")
             self.entry.delete(0, tk.END)
 
 if __name__ == "__main__":
